af4adf23dc2cb6ceac4bc8c15780cc5619798098
Kevin Nolasco <kevnolasco95@gmail.com>
2022-07-05 21:14:05 -0700

    boxplots, binning, and histograms

--- Chapter1/EDA_stats.py
+++ Chapter1/EDA_stats.py
@@ -4,6 +4,7 @@ from scipy.stats import trim_mean
 import numpy as np
 import wquantiles
 from statsmodels import robust
+import matplotlib.pyplot as plt
 
 DATA_PATH = 'data'
 
@@ -39,3 +40,41 @@ MAD = robust.scale.mad(state['Population'])
 print('Standard Deviation : {:,.2f}'.format(stand_d))
 print('IQR : {:,.2f}'.format(IQR))
 print('MAD : {:,.2f}'.format(MAD))
+
+# percentiles with pandas
+print('\n')
+print(state['Murder.Rate'].quantile([0.05, 0.25, 0.5, 0.75, 0.95]))
+
+# using pandas to create a boxplot
+ax = (state['Population']/1_000_000).plot.box()
+ax.set_ylabel('Population (millions)')
+# need to call plt to show the graph
+plt.tight_layout()
+plt.show()
+
+# frequencies and histograms
+# use pandas.cut() with value_counts to bin continuous data
+print('\n')
+binned_population = pd.cut(state['Population'], bins = 10)
+print(binned_population.value_counts())
+
+# code to create table 1.5
+binned_population.name = 'binnedPopulation'
+df = pd.concat([state, binned_population], axis = 1) # add columns to states df
+df = df.sort_values(by = 'Population')
+
+# use group by for the for loop
+groups = []
+for group, subset in df.groupby(by = 'binnedPopulation'):
+    groups.append({
+        'BinRange' : group,
+        'Count' : len(subset),
+        'States' : ', '.join(subset['Abbreviation'])
+    })
+print('\n', pd.DataFrame(groups))
+
+# plot a histogram with pandas
+ax = (state['Population'] / 100_000_000).plot.hist(figsize = (4,4))
+ax.set_xlabel('Population(millions)')
+plt.tight_layout()
+plt.show()
